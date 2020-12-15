366a88baf15dec8841e7709e29065fa0ad12d5c6
hakase-monoshiri <ebenaeleazer@outlook.com>
2020-12-15 17:28:43 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+    i = 0
+    modified_array = []
+    while i < array.length
+        value = yield(array[i])
+        modified_array << value
+        i += 1
+    end
+    return modified_array
+end
 
 
